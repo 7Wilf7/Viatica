6cff1461ec2aec2dd24b05b5d7f0ce9080bf57e0
Wilf <wilf7wufan@gmail.com>
2026-07-07 11:25:35 +0800

    Make category Thiings icons unique

--- src/assets/thiings/index.js
+++ src/assets/thiings/index.js
@@ -1,55 +1,129 @@
 import airplaneUrl from "./airplane.png";
+import appleUrl from "./apple.png";
+import bandAidUrl from "./bandAid.png";
+import bicycleUrl from "./bicycle.png";
 import bookUrl from "./book.png";
 import boxUrl from "./box.png";
 import briefcaseUrl from "./briefcase.png";
+import busUrl from "./bus.png";
+import calendarUrl from "./calendar.png";
 import carUrl from "./car.png";
 import cashUrl from "./cash.png";
+import chatBubbleUrl from "./chatBubble.png";
+import chipsUrl from "./chips.png";
+import cloudUrl from "./cloud.png";
 import coffeeUrl from "./coffee.png";
-import creditCardUrl from "./creditCard.png";
+import croissantUrl from "./croissant.png";
+import desktopComputerUrl from "./desktopComputer.png";
+import diceUrl from "./dice.png";
+import dumbbellUrl from "./dumbbell.png";
+import fileUrl from "./file.png";
+import forkUrl from "./fork.png";
 import gameControllerUrl from "./gameController.png";
 import gearUrl from "./gear.png";
-import giftUrl from "./gift.png";
+import graduationCapUrl from "./graduationCap.png";
 import hamburgerUrl from "./hamburger.png";
 import heartUrl from "./heart.png";
+import hotelUrl from "./hotel.png";
 import houseUrl from "./house.png";
+import keyUrl from "./key.png";
+import mobilePhoneUrl from "./mobilePhone.png";
 import moneyUrl from "./money.png";
+import moonUrl from "./moon.png";
+import movieUrl from "./movie.png";
+import openBookUrl from "./openBook.png";
+import partyPopperUrl from "./partyPopper.png";
 import phoneUrl from "./phone.png";
+import pillBottleUrl from "./pillBottle.png";
+import plateUrl from "./plate.png";
+import proteinBarUrl from "./proteinBar.png";
+import questionMarkUrl from "./questionMark.png";
+import ramenUrl from "./ramen.png";
+import receiptUrl from "./receipt.png";
+import redEnvelopeUrl from "./redEnvelope.png";
 import refundUrl from "./refund.png";
+import restaurantUrl from "./restaurant.png";
 import robotUrl from "./robot.png";
+import sandwichUrl from "./sandwich.png";
 import scissorsUrl from "./scissors.png";
 import shieldUrl from "./shield.png";
 import shoppingBagUrl from "./shoppingBag.png";
+import shoppingBasketUrl from "./shoppingBasket.png";
 import sofaUrl from "./sofa.png";
+import stethoscopeUrl from "./stethoscope.png";
+import stopwatchUrl from "./stopwatch.png";
 import subscriptionUrl from "./subscription.png";
-import suitcaseUrl from "./suitcase.png";
+import tShirtUrl from "./tShirt.png";
 import taxiUrl from "./taxi.png";
+import teacherUrl from "./teacher.png";
+import ticketUrl from "./ticket.png";
+import toolboxUrl from "./toolbox.png";
 import trainUrl from "./train.png";
+import trophyUrl from "./trophy.png";
+import walletUrl from "./wallet.png";
 
 export const thiingsIconUrls = {
   airplane: airplaneUrl,
+  apple: appleUrl,
+  bandAid: bandAidUrl,
+  bicycle: bicycleUrl,
   book: bookUrl,
   box: boxUrl,
   briefcase: briefcaseUrl,
+  bus: busUrl,
+  calendar: calendarUrl,
   car: carUrl,
   cash: cashUrl,
+  chatBubble: chatBubbleUrl,
+  chips: chipsUrl,
+  cloud: cloudUrl,
   coffee: coffeeUrl,
-  creditCard: creditCardUrl,
+  croissant: croissantUrl,
+  desktopComputer: desktopComputerUrl,
+  dice: diceUrl,
+  dumbbell: dumbbellUrl,
+  file: fileUrl,
+  fork: forkUrl,
   gameController: gameControllerUrl,
   gear: gearUrl,
-  gift: giftUrl,
+  graduationCap: graduationCapUrl,
   hamburger: hamburgerUrl,
   heart: heartUrl,
+  hotel: hotelUrl,
   house: houseUrl,
+  key: keyUrl,
+  mobilePhone: mobilePhoneUrl,
   money: moneyUrl,
+  moon: moonUrl,
+  movie: movieUrl,
+  openBook: openBookUrl,
+  partyPopper: partyPopperUrl,
   phone: phoneUrl,
+  pillBottle: pillBottleUrl,
+  plate: plateUrl,
+  proteinBar: proteinBarUrl,
+  questionMark: questionMarkUrl,
+  ramen: ramenUrl,
+  receipt: receiptUrl,
+  redEnvelope: redEnvelopeUrl,
   refund: refundUrl,
+  restaurant: restaurantUrl,
   robot: robotUrl,
+  sandwich: sandwichUrl,
   scissors: scissorsUrl,
   shield: shieldUrl,
   shoppingBag: shoppingBagUrl,
+  shoppingBasket: shoppingBasketUrl,
   sofa: sofaUrl,
+  stethoscope: stethoscopeUrl,
+  stopwatch: stopwatchUrl,
   subscription: subscriptionUrl,
-  suitcase: suitcaseUrl,
+  tShirt: tShirtUrl,
   taxi: taxiUrl,
+  teacher: teacherUrl,
+  ticket: ticketUrl,
+  toolbox: toolboxUrl,
   train: trainUrl,
+  trophy: trophyUrl,
+  wallet: walletUrl,
 };
